f2c790e018a4d151ec88fd39c0b49999336e6a5d
Sang Dang <sangdth@gmail.com>
2020-12-18 17:42:51 +0200

    local package tsconfig just ignored

--- packages/builder/lib/builder.js
+++ packages/builder/lib/builder.js
@@ -1,101 +1,60 @@
 #!/usr/bin/env node
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var rollup = require('rollup');
-var path = require('path');
-var resolve = require('@rollup/plugin-node-resolve')["default"];
-var babel = require('@rollup/plugin-babel')["default"];
-// const typescript = require('@rollup/plugin-typescript');
-var currentWorkingPath = process.cwd();
+
+const rollup = require('rollup');
+const path = require('path');
+const resolve = require('@rollup/plugin-node-resolve').default;
+const babel = require('@rollup/plugin-babel').default;
+const typescript = require('@rollup/plugin-typescript');
+
+const currentWorkingPath = process.cwd();
+
 /* eslint-disable-next-line */ // TODO: Fix the next require
-var _a = require(path.join(currentWorkingPath, 'package.json')), src = _a.src, pkgName = _a.name;
+const {src, name: pkgName} = require(path.join(currentWorkingPath, 'package.json'));
+
 // get the current working dir (button, card, etc)
-var inputPath = path.join(currentWorkingPath, src);
+const inputPath = path.join(currentWorkingPath, src);
+
 // Little workaround to get package name without scope
-var fileName = pkgName.replace('@rete-academy/', '');
+const fileName = pkgName.replace('@rete-academy/', '');
+
 // see below for details on the options
-var inputOptions = {
+const inputOptions = {
     input: inputPath,
     external: ['react'],
     plugins: [
-        // typescript(),
+        typescript({
+            // tsconfig: path.join(currentWorkingPath, 'tsconfig.json'),
+            tsconfig: './tsconfig.json',
+        }),
         babel({
             presets: ['@babel/preset-env', '@babel/preset-react'],
             babelHelpers: 'bundled',
-            exclude: 'node_modules/**'
+            exclude: 'node_modules/**',
         }),
         resolve(),
-    ]
+    ],
 };
-var outputOptions = [
+
+const outputOptions = [
     {
-        file: "dist/" + fileName + ".cjs.js",
+        file: `dist/${fileName}.cjs.js`,
         format: 'cjs',
-        exports: 'named'
+        exports: 'named',
     },
     {
-        file: "dist/" + fileName + ".esm.js",
+        file: `dist/${fileName}.esm.js`,
         format: 'esm',
-        exports: 'named'
+        exports: 'named',
     },
 ];
-function build() {
-    return __awaiter(this, void 0, void 0, function () {
-        var bundle;
-        var _this = this;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, rollup.rollup(inputOptions)];
-                case 1:
-                    bundle = _a.sent();
-                    // loop through the options and write individual bundles
-                    outputOptions.forEach(function (options) { return __awaiter(_this, void 0, void 0, function () {
-                        return __generator(this, function (_a) {
-                            switch (_a.label) {
-                                case 0: return [4 /*yield*/, bundle.write(options)];
-                                case 1:
-                                    _a.sent();
-                                    return [2 /*return*/];
-                            }
-                        });
-                    }); });
-                    return [2 /*return*/];
-            }
-        });
+
+async function build() {
+    // create bundle
+    const bundle = await rollup.rollup(inputOptions);
+    // loop through the options and write individual bundles
+    outputOptions.forEach(async (options) => {
+        await bundle.write(options);
     });
 }
+
 build();
--- packages/button/docs/button.stories.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-declare const _default: {
-    title: string;
-};
-export default _default;
-export declare const primary: () => JSX.Element;
-//# sourceMappingURL=button.stories.d.ts.map
